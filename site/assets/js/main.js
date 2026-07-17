7b810fd9a919eb5818173041580d5bafec749eb8
CristianVR360 <cristiantrivinom@gmail.com>
2026-07-17 17:28:45 -0400

    correcciones varias

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -247,18 +247,7 @@ $(function () {
         removalDelay: 0,
         type: 'iframe',
         iframe: {
-            markup: '<div class="mfp-iframe-scaler">' +
-                '<div class="mfp-close"></div>' +
-                '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
-                '</div>',
-            patterns: {
-                youtube: {
-                    index: 'youtube.com/',
-                    id: 'v=',
-                    src: 'https://www.youtube.com/embed/%id%?autoplay=1'
-                }
-            },
-            srcAction: 'src',
+            patterns: {} // Bypass default parsers (avoiding undefined video ID parsing)
         }
     });
 
